a39c4f3b80689b34d40b71af0d5e9468a980c24e
Almeida <101214869+polarialm@users.noreply.github.com>
2022-12-06 16:51:44 +0000

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const array = word.split("");
+    let result = "";
+    for (let x = array.length-1; x >= 0; x--) {
+        result += array[x];
+    } 
+    return result;
 };
 
 // Do not edit below this line

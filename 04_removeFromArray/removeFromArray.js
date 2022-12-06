a25ae9f1521fa5ea888209d5e1c49695253c7748
Almeida <101214869+polarialm@users.noreply.github.com>
2022-12-06 17:31:01 +0000

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...rem) {
+    // Look through the entire array for a certain variable, then once you find it, remove it
+    /*for (let x = 0; x < array.length-1; x++) {
+        if (array[x] == rem) {
+            array.splice(x,1)
+        }
+    }*/
+    rem.forEach((element, index) => {
+        for (let x = 0; x < array.length; x++) {
+            if (array[x] === element) {
+                array.splice(x,1)
+            }}
+    })
+    return array;
 };
 
 // Do not edit below this line

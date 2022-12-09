0b9397f4477ac54de4891b06b599cbf8b3ae6e99
Almeida <101214869+polarialm@users.noreply.github.com>
2022-12-09 17:26:14 +0000

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(arg) {
+  let result = Math.round((((arg - 32) * (5/9)))*10)/10
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(arg) {
+  let result = Math.round((arg * (9/5) + 32)*10)/10
+  return result
 };
 
 // Do not edit below this line
